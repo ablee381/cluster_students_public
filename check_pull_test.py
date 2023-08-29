58a75b7b8123f3836b2f34d2254f6355538c8ead
Alexander Lee <ablee381@gmail.com>
2023-08-29 14:11:47 -0400

    main.py can be used to run the code in the command line

--- check_pull_test.py
+++ check_pull_test.py
@@ -33,32 +33,4 @@ def check_status(pdf_id):
 
 if __name__ == '__main__':
     print(check_status('2023_08_28_566d224c9a5c598df557g'))
-    # f = open('demo/pdf_id.txt', 'r')
-    # line = f.readline().strip()
-    # lineParts = line.split('\t')
-    # print(lineParts)
-    # assert(len(lineParts) == 2)
-    # test_name = lineParts[0]
-    # pdf_id = lineParts[1].split(':')[1][1:-2]
-    # load_latex(test_name, pdf_id, outDir='demo')
 
-    # # pdf_id.txt has 55 lines. I counted and that is what I was expecting.
-    # # I don't want infinite looping api calls, so no while loops
-    # for i in range(55):
-    #     line = f.readline().strip()
-    #     lineParts = line.split('\t')
-    #     print(lineParts)
-    #     assert(len(lineParts) == 2)
-    #     standard = lineParts[0]
-    #     pdf_id = lineParts[1].split(':')[1][1:-2]
-    #     load_latex(standard, pdf_id)
-    #     time.sleep(1.5)
-
-# # Check status of conversion
-# json_dict = requests.get('https://api.mathpix.com/v3/converter/2023_07_03_01209c7a1a0964e85bb5g',
-#                          headers={
-#                              "app_id": os.getenv('MATHPIX_ID'),
-#                              "app_key": os.getenv('MATHPIX_KEY')
-#                          }
-#                          ).json()
-# print('status', json_dict)
